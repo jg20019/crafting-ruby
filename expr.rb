09748ab7671d6d33de1ccb7dd3f7ab2b84862cf8
John Gibson <jg20019@gmail.com>
2022-05-03 20:53:54 -0400

    Manually added constructors

--- expr.rb
+++ expr.rb
@@ -19,6 +19,12 @@ end
 
 class Binary < Expr
     attr_accessor :left, :operator, :right
+    def initialize(left, operator, right)
+      @left = left
+      @operator = operator
+      @right = right
+    end
+
     def accept(visitor)
         visitor.visitBinaryExpr(self)
     end
@@ -26,6 +32,10 @@ end
 
 class Grouping < Expr
     attr_accessor :expression
+    def initialize(expression)
+      @expression = expression
+    end
+
     def accept(visitor)
         visitor.visitGroupingExpr(self)
     end
@@ -33,6 +43,10 @@ end
 
 class Literal < Expr
     attr_accessor :value
+    def initialize(value)
+      @value = value
+    end
+
     def accept(visitor)
         visitor.visitLiteralExpr(self)
     end
@@ -40,6 +54,11 @@ end
 
 class Unary < Expr
     attr_accessor :operator, :right
+    def initialize(operator, right)
+      @operator = operator
+      @right = right
+    end
+
     def accept(visitor)
         visitor.visitUnaryExpr(self)
     end
