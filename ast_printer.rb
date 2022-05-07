a5d93b93cc186cf897e045d897239cea78619ca9
John Gibson <jg20019@gmail.com>
2022-05-07 12:02:15 -0400

    Remove main from ast printer

--- ast_printer.rb
+++ ast_printer.rb
@@ -33,17 +33,4 @@ class AstPrinter < Visitor
     builder << ")"
     builder.join
   end
-
-  def AstPrinter.main
-    expr = Binary.new(
-      Unary.new(
-        Token.new(TokenType::MINUS, "-", nil, 1),
-        Literal.new(123)),
-      Token.new(TokenType::STAR, "*", nil, 1),
-      Grouping.new(
-        Literal.new(45.67)))
-    puts AstPrinter.new.print(expr)
-  end
 end
-
-AstPrinter.main
